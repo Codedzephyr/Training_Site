3b1130fd17e3eb7239d70fc07b1aaae75bfc143c
Codedzephyr <salihu45@gmail.com>
2022-05-14 06:58:25 +0100

    done

--- src/Components/FrontPage.jsx
+++ src/Components/FrontPage.jsx
@@ -129,7 +129,7 @@ const FrontPage = () => {
                         width="40"
                         height="40"
                         xmlns="http://www.w3.org/2000/svg"
-                        enable-background="new 0 0 24 24"
+                        enableBackground="new 0 0 24 24"
                         viewBox="0 0 24 24"
                       >
                         <path
